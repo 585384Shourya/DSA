e8e048c35c5f964901c0269595b99d9f8b3f0cb2
shourya shandilya <shandishourya@gmail.com>
2026-01-26 21:23:02 +0530

    Removing element(LC 27)

---
+++ remove_element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums.length==0)return 0;
+        int i=0,j=0;
+        while(j!=nums.length){
+            if(nums[j]==val){
+                j++;
+            }
+            else if(nums[j]!=val){
+                nums[i]=nums[j];
+                i++;
+                j++;
+            }
+        }
+        return i;
+    }
+}
